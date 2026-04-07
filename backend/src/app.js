f32a1839845bc3e72a7b39a788e5de90ab408a6e
Yuraev Aleksandr <podsolnyx03@mail.ru>
2026-04-07 11:40:26 +0700

    Добавил новые

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
 const cookieParser = require('cookie-parser');
-const { register, metricsMiddleware } = require('./metrics');
+const { client, register, metricsMiddleware } = require('./metrics');
 
 const coreAuthRouter = require('./routes/coreRoutes/coreAuth');
 const coreApiRouter = require('./routes/coreRoutes/coreApi');
--- backend/src/metrics.js
+++ backend/src/metrics.js
@@ -4,13 +4,18 @@ const register = new client.Registry();
 
 client.collectDefaultMetrics({ register });
 
+/* ---------------- HTTP LATENCY ---------------- */
+
 const httpRequestDuration = new client.Histogram({
     name: 'http_request_duration_seconds',
     help: 'Duration of HTTP requests in seconds',
     labelNames: ['method', 'route', 'status'],
+    buckets: [0.005, 0.01, 0.05, 0.1, 0.3, 0.5, 1, 2, 5],
 });
 
 register.registerMetric(httpRequestDuration);
+
+/* ---------------- HTTP REQUEST COUNT ---------------- */
 
 const httpRequestsTotal = new client.Counter({
     name: 'http_requests_total',
@@ -20,6 +25,8 @@ const httpRequestsTotal = new client.Counter({
 
 register.registerMetric(httpRequestsTotal);
 
+/* ---------------- HTTP ERRORS ---------------- */
+
 const httpErrorsTotal = new client.Counter({
     name: 'http_errors_total',
     help: 'Total number of HTTP errors',
@@ -28,14 +35,7 @@ const httpErrorsTotal = new client.Counter({
 
 register.registerMetric(httpErrorsTotal);
 
-const httpResponseSize = new client.Histogram({
-    name: 'http_response_size_bytes',
-    help: 'Size of HTTP responses in bytes',
-    labelNames: ['method', 'route', 'status'],
-    buckets: [100, 500, 1000, 5000, 10000, 50000],
-});
-
-register.registerMetric(httpResponseSize);
+/* ---------------- ACTIVE REQUESTS ---------------- */
 
 const httpActiveRequests = new client.Gauge({
     name: 'http_active_requests',
@@ -44,10 +44,45 @@ const httpActiveRequests = new client.Gauge({
 
 register.registerMetric(httpActiveRequests);
 
+/* ---------------- RESPONSE SIZE ---------------- */
+
+const httpResponseSize = new client.Histogram({
+    name: 'http_response_size_bytes',
+    help: 'Size of HTTP responses in bytes',
+    labelNames: ['method', 'route', 'status'],
+    buckets: [100, 500, 1000, 5000, 10000, 50000, 100000],
+});
+
+register.registerMetric(httpResponseSize);
+
+/* ---------------- REQUEST SIZE ---------------- */
+
+const httpRequestSize = new client.Histogram({
+    name: 'http_request_size_bytes',
+    help: 'Size of HTTP request body',
+    labelNames: ['method', 'route'],
+    buckets: [100, 500, 1000, 5000, 10000, 50000],
+});
+
+register.registerMetric(httpRequestSize);
+
+/* ---------------- MIDDLEWARE ---------------- */
+
 function metricsMiddleware(req, res, next) {
     const end = httpRequestDuration.startTimer();
 
     httpActiveRequests.inc();
+
+    const requestSize = req.headers['content-length'];
+    if (requestSize) {
+        httpRequestSize.observe(
+            {
+                method: req.method,
+                route: req.path,
+            },
+            Number(requestSize)
+        );
+    }
 
     res.on('finish', () => {
         const labels = {
@@ -57,10 +92,16 @@ function metricsMiddleware(req, res, next) {
         };
 
         end(labels);
+
         httpRequestsTotal.inc(labels);
 
         if (res.statusCode >= 400) {
             httpErrorsTotal.inc(labels);
+        }
+
+        const responseSize = res.getHeader('content-length');
+        if (responseSize) {
+            httpResponseSize.observe(labels, Number(responseSize));
         }
 
         httpActiveRequests.dec();
@@ -69,4 +110,8 @@ function metricsMiddleware(req, res, next) {
     next();
 }
 
-module.exports = { register, metricsMiddleware };
+module.exports = {
+    client,
+    register,
+    metricsMiddleware,
+};
