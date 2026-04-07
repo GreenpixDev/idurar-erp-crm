cb0fd79624b053f47233c54c653cb013277f92f7
Yuraev Aleksandr <podsolnyx03@mail.ru>
2026-04-07 11:19:28 +0700

    Вынес метрики в отдельны файл и добавил новые

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
 const cookieParser = require('cookie-parser');
-const { client, register } = require('./metrics');
+const { register, metricsMiddleware } = require('./metrics');
 
 const coreAuthRouter = require('./routes/coreRoutes/coreAuth');
 const coreApiRouter = require('./routes/coreRoutes/coreApi');
@@ -17,18 +17,6 @@ const erpApiRouter = require('./routes/appRoutes/appApi');
 const fileUpload = require('express-fileupload');
 // create our Express app
 const app = express();
-
-//-------------- Добавляем метрики ------------------------
-
-const httpRequestDuration = new client.Histogram({
-  name: 'http_request_duration_seconds',
-  help: 'Duration of HTTP requests in seconds',
-  labelNames: ['method', 'route', 'status'],
-});
-
-register.registerMetric(httpRequestDuration);
-
-//---------------------------------------------------------------
 
 app.use(
   cors({
@@ -46,21 +34,8 @@ app.use(compression());
 // // default options
 // app.use(fileUpload());
 
-
 // Устанавливаем метрики HTTP запросов
-app.use((req, res, next) => {
-  const end = httpRequestDuration.startTimer();
-
-  res.on('finish', () => {
-    end({
-      method: req.method,
-      route: req.route?.path || req.path,
-      status: res.statusCode,
-    });
-  });
-
-  next();
-});
+app.use(metricsMiddleware);
 
 // Запрос на метрики
 app.get('/metrics', async (req, res) => {
--- backend/src/metrics.js
+++ backend/src/metrics.js
@@ -4,4 +4,69 @@ const register = new client.Registry();
 
 client.collectDefaultMetrics({ register });
 
-module.exports = { client, register };
+const httpRequestDuration = new client.Histogram({
+    name: 'http_request_duration_seconds',
+    help: 'Duration of HTTP requests in seconds',
+    labelNames: ['method', 'route', 'status'],
+});
+
+register.registerMetric(httpRequestDuration);
+
+const httpRequestsTotal = new client.Counter({
+    name: 'http_requests_total',
+    help: 'Total number of HTTP requests',
+    labelNames: ['method', 'route', 'status'],
+});
+
+register.registerMetric(httpRequestsTotal);
+
+const httpErrorsTotal = new client.Counter({
+    name: 'http_errors_total',
+    help: 'Total number of HTTP errors',
+    labelNames: ['method', 'route', 'status'],
+});
+
+register.registerMetric(httpErrorsTotal);
+
+const httpResponseSize = new client.Histogram({
+    name: 'http_response_size_bytes',
+    help: 'Size of HTTP responses in bytes',
+    labelNames: ['method', 'route', 'status'],
+    buckets: [100, 500, 1000, 5000, 10000, 50000],
+});
+
+register.registerMetric(httpResponseSize);
+
+const httpActiveRequests = new client.Gauge({
+    name: 'http_active_requests',
+    help: 'Number of active HTTP requests',
+});
+
+register.registerMetric(httpActiveRequests);
+
+function metricsMiddleware(req, res, next) {
+    const end = httpRequestDuration.startTimer();
+
+    httpActiveRequests.inc();
+
+    res.on('finish', () => {
+        const labels = {
+            method: req.method,
+            route: req.route?.path || req.path,
+            status: res.statusCode,
+        };
+
+        end(labels);
+        httpRequestsTotal.inc(labels);
+
+        if (res.statusCode >= 400) {
+            httpErrorsTotal.inc(labels);
+        }
+
+        httpActiveRequests.dec();
+    });
+
+    next();
+}
+
+module.exports = { register, metricsMiddleware };
